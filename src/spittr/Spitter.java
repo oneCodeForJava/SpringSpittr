eb5fb57f8ddb4444afa80277c13143f8a01e2251
LiXiangForJava <1208260820@qq.com>
2018-07-12 17:11:23 +0800

    增加实体校验功能

--- src/spittr/Spitter.java
+++ src/spittr/Spitter.java
@@ -1,13 +1,24 @@
 package spittr;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 
 public class Spitter {
 	private  Long id;
+	@NotNull
+	@Size(min=2, max=30)
 	private  String firstName;
+	@NotNull
+	@Size(min=2, max=30)
 	private  String lastName;
+	@NotNull
+	@Size(min=5, max=16)
 	private  String username;
+	@NotNull
+	@Size(min=5, max=25)
 	private  String password;
 	
 	
--- src/spittr/web/SpitterController.java
+++ src/spittr/web/SpitterController.java
@@ -1,8 +1,11 @@
 package spittr.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,7 +30,10 @@ public class SpitterController {
 	}
 	
 	@RequestMapping(value="/register", method=RequestMethod.POST)
-	public String processRegisttation(Spitter spitter){
+	public String processRegisttation(@Valid Spitter spitter, Errors errors){
+		if(errors.hasErrors()){
+			return "registerForm";
+		}
 		spittleRepository.save(spitter);
 		return "redirect:/spitter/"+spitter.getUsername();
 	}
